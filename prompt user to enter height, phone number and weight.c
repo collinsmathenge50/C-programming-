446abecff8572c87b6520195e0fdfe5e934c0648
collinsmathenge50 <collinsmathenge50@gmail.com>
2025-09-24 23:49:15 +0300

    Add files via upload

---
+++ prompt user to enter height, phone number and weight.c
@@ -0,0 +1,30 @@
+/*
+Name:mathenge Collins mureithi
+ Registration number:CT100/G/26138/25
+ description:program to prompt the user to enter:
+          1.height
+          2.weight
+          3.phonenumber
+ */
+ #include<stdio.h>
+ 
+ int main() {
+ 
+ // prompting user to enter height
+ int height;
+ printf("enter your height:");
+ scanf("%d",&height);
+ 
+ //prompting user to enter weight
+ int weight;
+ printf("enter weight:");
+ scanf("%d",&weight);
+ 
+ //prompting user to enter phonenumber
+ int phonenumber;
+ printf("enter phonenumber;");
+ scanf("%d",&phonenumber);
+ 
+ 
+ return 0;
+ }
